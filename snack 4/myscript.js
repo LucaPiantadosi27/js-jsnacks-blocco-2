74637e70dfca9db022d1e75350c633674dc2ed00
Luca Piantadosi <lu.piantadosi125@gmail.com>
2024-02-07 19:35:27 +0100

    snack 5

---
+++ snack 4/myscript.js
@@ -0,0 +1,35 @@
+// Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo
+
+let userChoice = prompt("Scegli un numero, oppure clicca 'exit'");
+
+
+console.log("user choice", userChoice);
+
+
+// finchè la scelta è diversa da "exit"
+
+while (userChoice != "exit") {
+
+    if ( ! isNaN(userChoice)) { // NOT, solo se  è un numero
+
+        // converto in numero la scelta dell'utente
+        const userNumber = Number(userChoice)
+
+        // faccio il controllo
+        if (userNumber % 2 == 0) { // pari
+
+            console.log(userNumber);
+        
+        } else {
+        
+            console.log(userNumber + 1);
+        }
+
+    } else {
+        alert("Inserisci un numero");
+    }
+
+    userChoice = prompt("Scegli un altro numero, oppure clicca 'exit'")
+}
+
+console.log("uscita")
